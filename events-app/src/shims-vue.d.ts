1c8582c995c0334648e29287246570a6d77e24c5
Kilian Testard <pc47xiy@eduvaud.ch>
2025-12-15 11:36:36 +0100

    exercise 4 project initialization

--- events-app/src/shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-// Permet à TypeScript d'importer correctement les fichiers .vue
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
