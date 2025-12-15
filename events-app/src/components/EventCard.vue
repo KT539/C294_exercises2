be5a0fd9ea1579300ba6adbfa36cd9a59225ed2a
Kilian Testard <pc47xiy@eduvaud.ch>
2025-12-15 10:05:32 +0100

    feat: eventcards functionnal, starting with routing

---
+++ events-app/src/components/EventCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="event-card">
+    <span>{{ event.date }} à {{ event.time }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<!-- scoped parameter: the css will apply to elements of the current component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  text-align: center;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ events-app/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home-view">
+    <h1>Upcoming Events</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+const events = ref([
+  {
+    id: 1,
+    time: '18:00',
+    date: '2025-12-10',
+    title: 'Soirée jeux de société',
+  },
+  {
+    id: 2,
+    time: '14:30',
+    date: '20205-12-15',
+    title: 'Atelier Vue 3',
+  },
+  {
+    id: 3,
+    time: '09:00',
+    date: '2025-12-20',
+    title: 'Petit-déjeuner en classe',
+  },
+])
+</script>
+
+<style scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
